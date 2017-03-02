3c7cd5a68a0e7ba15b69c68cba78b6df4102d3f1
SahilDShah <sahilshah193@gmail.com>
2017-03-02 22:22:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,19 +3,19 @@ var button = document.getElementById('counter');
 var counter=0;
 button.onclick = function() {
   counter = counter + 1;
-  /*var request = new XMLHttpRequest();
+  var request = new XMLHttpRequest();
   request.onreadystatechange = function() {
       if(request.readystate === XMLHttpRequest.DONE) {
           if(request.status === 200) {
-               var counter = request.responseText;*/
+               var counter = request.responseText;
                var span = document.getElementById('count');
                span.innerHTML = counter.toString();
                
-         // }
-      //}
+         }
+      }
       
- // };
+  };
   
- // request.open('GET','http://sahildshah.imad.hasura-app.io/', true);
-  // request.send(null);
+  request.open('GET','http://sahildshah.imad.hasura-app.io/', true);
+   request.send(null);
 };
