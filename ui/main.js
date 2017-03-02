05b748c04dde990fd9830b9522e3d310aedc3967
SahilDShah <sahilshah193@gmail.com>
2017-03-02 20:28:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element=document.getElementByID('main-text');
 element.innerHTML='This Is New Content';
+
+var img=document.getElementByID('madi');
+img.onclick = function() {
+  img.style.marginLeft='100px';   
+};
